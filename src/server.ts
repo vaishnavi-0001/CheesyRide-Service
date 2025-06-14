e863b0cd7a6d88ac597fb2415c986e8f0fb10215
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-14 17:09:55 +0530

    Include Logger

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
-         
-        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-    } catch (err) {
-         
-        console.error(err);
-        process.exit(1);
+        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
 
