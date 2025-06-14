64bb0fcb09b7a4f2bcf05795b49dbad6e5ec90b7
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-14 16:31:20 +0530

    ..

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
-import eslintJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslintJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  eslintJs.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    ignores: ['**/dist/**', 'node_modules',  'eslint.config.mjs'],
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    eslintJs.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ["**/dist/**", "node_modules", "eslint.config.mjs"],
     },
-    rules: {
-      // add your custom rules here
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            // add your custom rules here
+        },
     },
-  }
 );
