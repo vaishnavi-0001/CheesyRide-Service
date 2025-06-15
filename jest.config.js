7da5c89c817e046ec380bc91f31e3c1860255d0c
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-15 23:45:41 +0530

    format doc

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  verbose: true,
+    preset: "ts-jest",
+    testEnvironment: "node",
+    verbose: true,
 
-  // ✅ Only run .ts or .tsx test files
-  testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
+    // ✅ Only run .ts or .tsx test files
+    testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
 
-  // ✅ Ignore all JS files and dist folder
-  testPathIgnorePatterns: ["/node_modules/", "/dist/", "\\.js$"],
+    // ✅ Ignore all JS files and dist folder
+    testPathIgnorePatterns: ["/node_modules/", "/dist/", "\\.js$"],
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
-import path from "path"
+import path from "path";
 
 config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
