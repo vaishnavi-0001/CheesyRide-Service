b41c1de6e8d70d6bee8fde4bc8c50a9a1e586843
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-17 20:22:14 +0530

    ..

--- jest.config.js
+++ jest.config.js
@@ -3,21 +3,20 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     verbose: true,
-  
+
     // ✅ Only run .ts or .tsx test files
     testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
-  
+
     // ✅ Ignore all JS files and dist folder
     testPathIgnorePatterns: ["/node_modules/", "/dist/", "\\.js$"],
-  
+
     // ✅ Tell Jest to transform specific ESM packages in node_modules
     transformIgnorePatterns: [
-      "node_modules/(?!(mock-jwks|@peculiar/webcrypto|asn1.js|node-rsa)/)",
+        "node_modules/(?!(mock-jwks|@peculiar/webcrypto|asn1.js|node-rsa)/)",
     ],
-  
+
     // ✅ Use ts-jest to transform TypeScript files
     transform: {
-      "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.tsx?$": "ts-jest",
     },
-  };
-  
+};
