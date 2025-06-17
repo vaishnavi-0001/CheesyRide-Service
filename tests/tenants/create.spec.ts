70dac0ca86aafa21fbdd146dceff23e171ea544e
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-18 00:24:37 +0530

    Create Tenant Functionality

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import request from "supertest";
 import { AppDataSource } from "../../src/config/data-source";
 import app from "../../src/app";
+import { Tenant } from "../../src/entity/Tenant";
 
 describe("POST /tenants", () => {
     let connection: DataSource;
@@ -30,6 +31,21 @@ describe("POST /tenants", () => {
                 .send(tenantData);
 
             expect(response.statusCode).toBe(201);
+        });
+
+        it("should create a tenant in the database", async () => {
+            const tenantData = {
+                name: "Tenant name",
+                address: "Tenant address",
+            };
+
+            await request(app).post("/tenants").send(tenantData);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+            expect(tenants).toHaveLength(1);
+            expect(tenants[0].name).toBe(tenantData.name);
+            expect(tenants[0].address).toBe(tenantData.address);
         });
     });
 });
