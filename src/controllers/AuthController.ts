d143543982a7513c401f76db4af901ce240541bf
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-17 16:25:26 +0530

    Status Code changed

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -148,7 +148,7 @@ export class AuthController {
             });
 
             this.logger.info("User has been logged in", { id: user.id });
-            res.status(201).json({ id: user.id });
+            res.json({ id: user.id });
         } catch (err) {
             next(err);
             return;
