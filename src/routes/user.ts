20897e7a5608cd998f706bef179457feef056999
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-20 00:25:16 +0530

    ..

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, RequestHandler, Response } from "express";
+import express, { NextFunction, Response } from "express";
 import authenticate from "../middlewares/authenticate";
 import { canAccess } from "../middlewares/canAccess";
 import { Roles } from "../constants";
@@ -8,7 +8,7 @@ import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
 import logger from "../config/logger";
 import createUserValidator from "../validators/create-user-validatot";
-import { CreateUserRequest, UpdateUserRequest } from "../types";
+import { UpdateUserRequest } from "../types";
 import updateUserValidator from "../validators/update-user-validator";
 
 const router = express.Router();
@@ -17,67 +17,48 @@ const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
 const userController = new UserController(userService, logger);
 
+function asyncHandler(
+    fn: (
+        req: express.Request,
+        res: Response,
+        next: NextFunction,
+    ) => Promise<unknown>,
+) {
+    return (req: express.Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    };
+}
+
 router.post(
     "/",
-    authenticate as RequestHandler,
+    authenticate,
     canAccess([Roles.ADMIN]),
     createUserValidator,
-    (req: CreateUserRequest, res: Response, next: NextFunction) => {
-        userController.create(req, res, next).catch(next);
-    },
+    asyncHandler((req: express.Request, res: Response, next: NextFunction) =>
+        userController.create(req, res, next),
+    ),
 );
 
 router.patch(
     "/:id",
-    authenticate as RequestHandler,
+    authenticate,
     canAccess([Roles.ADMIN]),
     updateUserValidator,
-    async (req: UpdateUserRequest, res: Response, next: NextFunction) => {
-        try {
-            await userController.update(req, res, next);
-        } catch (err) {
-            next(err);
-        }
-    },
+    asyncHandler((req: UpdateUserRequest, res: Response, next: NextFunction) =>
+        userController.update(req, res, next),
+    ),
+);
+router.get("/", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+    userController.getAll(req, res, next),
 );
 
-router.get(
-    "/",
-    authenticate as RequestHandler,
-    canAccess([Roles.ADMIN]),
-    async (req, res, next) => {
-        try {
-            await userController.getAll(req, res, next);
-        } catch (err) {
-            next(err);
-        }
-    },
+router.get("/:id", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+    userController.getOne(req, res, next),
 );
-
-router.get(
-    "/:id",
-    authenticate as RequestHandler,
-    canAccess([Roles.ADMIN]),
-    async (req, res, next) => {
-        try {
-            await userController.getOne(req, res, next);
-        } catch (err) {
-            next(err);
-        }
-    },
-);
-
 router.delete(
     "/:id",
-    authenticate as RequestHandler,
+    authenticate,
     canAccess([Roles.ADMIN]),
-    async (req, res, next) => {
-        try {
-            await userController.destroy(req, res, next);
-        } catch (err) {
-            next(err);
-        }
-    },
+    (req, res, next) => userController.destroy(req, res, next),
 );
-
 export default router;
